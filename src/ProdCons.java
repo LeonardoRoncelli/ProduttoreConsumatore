c9f5ec64400cc5993a42a107f20999ea165f7387
roncelli.23346 <roncelli.leonardo.studente@itispaleocapa.it>
2024-11-08 11:56:55 +0100

    Aggiunta metodo produce e prova di stampa. Con errori

--- src/ProdCons.java
+++ src/ProdCons.java
@@ -1,9 +1,22 @@
 import java.util.concurrent.TimeUnit;
+import java.util.Random;
 public class ProdCons extends Thread{
-    static int []prod=new int[100];
-
+    Random random=new Random();
+    static int []prodotti=new int[100];
+    public void produce(){
+        int elemento;
+        for(int i=0;i< prodotti.length;i++){
+            elemento=random.nextInt(100);
+            prodotti[i]=elemento;
+        }
+    }
+    public void stampa(){
+        for(int i=0;i<prodotti.length;i++) {
+            System.out.println(prodotti[i]);
+        }
+    }
     public void run(){
-
+        stampa();
     }
     public static void main(String[] args) {
         ProdCons produttore=new ProdCons();
