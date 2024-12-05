584035bb3f77f54e75a2341e62d9fcf68849ca3b
Roncelli <leonardoroncelli@gmail.com>
2024-12-05 18:05:22 +0100

    main nella classe TestProduttoreConsumatore.

--- src/TestProduttoreConsumatore.java
+++ src/TestProduttoreConsumatore.java
@@ -1,5 +1,12 @@
+import java.util.concurrent.Semaphore;
 public class TestProduttoreConsumatore {
     public static void main(String[]args){
-
+        int[]prodotti=new int[10];
+        Semaphore pieno=new Semaphore(0);
+        Semaphore vuoto=new Semaphore(prodotti.length);
+        Produttore prod=new Produttore(prodotti,"Produttore",vuoto,pieno);
+        Consumatore cons=new Consumatore(prodotti,"Consumatore",vuoto,pieno);
+        prod.start();
+        cons.start();
     }
 }
